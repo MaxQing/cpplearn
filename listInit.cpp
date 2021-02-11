3f998a10887f3fe52e0559de0889a16d5abb2a06
MaxQing <1191275859@qq.com>
2021-02-11 21:07:51 +0800

    current.

--- listInit.cpp
+++ listInit.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // if data will be dropped, the compiler will report errors.
     long double temp = 3.1415926;
     // int a{temp}; this is wrong. because the data will be dropped.
     int b(temp);
