48cecf0285d89d4df20f8b659e10465041a62a2b
MaxQing <1191275859@qq.com>
2021-02-22 15:28:03 +0800

    done.

---
+++ stringOperate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // string can use `<<` and `>>` to read and write from the std iostream.
+    // read unknown number of strings
+    // string word;
+    // while (cin >> word) {
+    //     cout << word << endl;
+    // }
+
+    // getline (gain a whole line, not contains the \n)
+    // string line;
+    // while (getline(cin, line))
+    //     cout << line << endl;
+
+    // empty and size
+    // size is a unsigned int, its type is std::size_type.
+    string temp;
+    cin >> temp;
+    if (!temp.empty()) {
+        cout << "The length of this string is " << temp.size() << endl;
+    }
+    return 0;
+}
