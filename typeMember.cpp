5baedf3661f46a440122bca5d0524cdc5bfc9d5b
MaxQing <1191275859@qq.com>
2021-05-09 15:27:26 +0800

    5.9 upgrade

---
+++ typeMember.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+
+class Screen {
+public:
+  // equals using pos = std::string::size_type;
+  typedef std::string::size_type pos;
+  Screen() = default;
+  Screen(pos ht, pos wd, char c)
+      : height(ht), width(wd), contents(ht * wd, c) {}
+  char get() const { return contents[cursor]; }
+  inline char get(pos ht, pos wd) const;
+  Screen &move(pos r, pos c);
+  Screen &set(char);
+  Screen &set(pos, pos, char);
+  Screen &display(std::ostream &os) {
+    do_display(os);
+    return *this;
+  }
+  const Screen &display(std::ostream &os) const {
+    do_display(os);
+    return *this;
+  }
+
+private:
+  pos cursor = 0;
+  pos height = 0, width = 0;
+  std::string contents;
+  void do_display(std::ostream &os) const { os << contents; }
+};
+
+// outside -> inline
+inline Screen &Screen::move(pos r, pos c) {
+  pos row = r * width;
+  cursor = row + c;
+  return *this;
+}
+
+char Screen::get(pos ht, pos wd) const {
+  pos row = ht * width;
+  return contents[row + wd];
+}
+
+inline Screen &Screen::set(char c) {
+  contents[cursor] = c;
+  return *this;
+}
+
+inline Screen &Screen::set(pos r, pos col, char ch) {
+  contents[r * width + col] = ch;
+  return *this;
+}
+
+class Window_mgr {
+private:
+  std::vector<Screen> screens{Screen(24, 80, ' ')};
+};
+
+int main() {
+  // overload member function
+  Screen myscreen;
+  // call Screen::get()
+  char ch = myscreen.get();
+  // call Screen::get(pos ht, pos wd)
+  char ch_2 = myscreen.get(0, 0);
+  return 0;
+}
