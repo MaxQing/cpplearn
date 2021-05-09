3fa51c0a444cb0d1c3f28748e41e7f52eefa7f8a
MaxQing <1191275859@qq.com>
2021-05-09 15:31:34 +0800

    5.9 upgrade

--- typeMember.cpp
+++ typeMember.cpp
@@ -57,11 +57,10 @@ private:
 };
 
 int main() {
-  // overload member function
-  Screen myscreen;
-  // call Screen::get()
-  char ch = myscreen.get();
-  // call Screen::get(pos ht, pos wd)
-  char ch_2 = myscreen.get(0, 0);
+  Screen myScreen(5, 5, 'X');
+  myScreen.move(4, 0).set('#').display(std::cout);
+  std::cout << "\n";
+  myScreen.display(std::cout);
+  std::cout << "\n";
   return 0;
 }
